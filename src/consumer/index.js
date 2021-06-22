7de8d85b5e14ba4c7143ba9c93925cdc2eff5701
Shriniket Sarkar <shriniket.sarkar@cerner.com>
2021-06-22 14:57:56 -0500

    Consuming xfc from xfc-1.10.1-onerror-fix with manually added polyfills. Missed the actual polyfill.

--- src/consumer/index.js
+++ src/consumer/index.js
@@ -1,3 +1,17 @@
 import Consumer from './consumer';
 
+if (!Object.entries) {
+  Object.entries = function (obj) {
+    const ownProps = Object.keys(obj);
+    let i = ownProps.length;
+    const resArray = new Array(i); // preallocate the Array
+    while (i) {
+      resArray[i] = [ownProps[i], obj[ownProps[i]]];
+      i -= 1;
+    }
+
+    return resArray;
+  };
+}
+
 export default new Consumer();
